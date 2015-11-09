e09122a9cdfcd8538d8b543ba27cfb6a510f07ca
Paul Colomiets <paul@colomiets.name>
2015-11-09 16:16:33 +0200

    Updated two_servers example

--- examples/two_servers.rs
+++ examples/two_servers.rs
@@ -1,29 +1,12 @@
-use std::io;
-use std::marker::PhantomData;
-
 extern crate hyper;
 extern crate rotor_http;
 extern crate rotor;
 extern crate mio;
 
+use rotor_http::HttpServer;
 
-type FSMIncr = rotor::transports::accept::Serve<
-                mio::tcp::TcpListener,
-                rotor::transports::greedy_stream::Stream<
-                    mio::tcp::TcpStream,
-                    rotor_http::http1::Client<Context, Incr>,
-                    Context>,
-                Context>;
-type FSMGet = rotor::transports::accept::Serve<
-                mio::tcp::TcpListener,
-                rotor::transports::greedy_stream::Stream<
-                    mio::tcp::TcpStream,
-                    rotor_http::http1::Client<Context, Get>,
-                    Context>,
-                Context>;
-type StateMachine = Wrapper<FSMIncr, FSMGet>;
 
-struct Context {
+pub struct Context {
     counter: usize,
 }
 
@@ -47,12 +30,6 @@ struct Incr;
 
 struct Get;
 
-enum Wrapper<A, B> {
-    Incr(A),
-    Get(B),
-}
-struct ScopeIncr<'a, S: 'a, A, B, C>(&'a mut S, PhantomData<*const (A, B, C)>);
-struct ScopeGet<'a, S: 'a, A, B, C>(&'a mut S, PhantomData<*const (A, B, C)>);
 
 impl<C:IncrCounter> rotor_http::http1::Handler<C> for Incr {
     fn request(req: rotor_http::http1::Request,
@@ -87,110 +64,49 @@ impl<C:GetCounter> rotor_http::http1::Handler<C> for Get {
     }
 }
 
-
-impl<'a, S, A, B, C> rotor::Scope<A> for ScopeIncr<'a, S, A, B, C>
-    where S: rotor::Scope<Wrapper<A, B>> + 'a,
-          A: rotor::EventMachine<C>,
-          B: rotor::EventMachine<C>,
-{
-    fn async_add_machine(&mut self, m: A) -> Result<(), A> {
-        self.0.async_add_machine(Wrapper::Incr(m))
-        .map_err(|x| if let Wrapper::Incr(c) = x {
-            c
-        } else {
-            unreachable!();
-        })
-    }
-    fn add_timeout_ms(&mut self, delay: u64, t: A::Timeout)
-        -> Result<mio::Timeout, mio::TimerError>
-    {
-        self.0.add_timeout_ms(delay, Wrapper::Incr(t))
-    }
-    fn clear_timeout(&mut self, timeout: mio::Timeout) -> bool {
-        self.0.clear_timeout(timeout)
-    }
-    fn register<E: ?Sized>(&mut self, io: &E,
-        interest: mio::EventSet, opt: mio::PollOpt)
-        -> Result<(), io::Error>
-        where E: mio::Evented
-    {
-        self.0.register(io, interest, opt)
-    }
+enum Wrapper<C:IncrCounter + GetCounter> {
+    Incr(HttpServer<C, Incr>),
+    Get(HttpServer<C, Get>),
 }
 
-impl<'a, S, A, B, C> rotor::Scope<B> for ScopeGet<'a, S, A, B, C>
-    where S: rotor::Scope<Wrapper<A, B>> + 'a,
-          A: rotor::EventMachine<C>,
-          B: rotor::EventMachine<C>,
-{
-    fn async_add_machine(&mut self, m: B) -> Result<(), B> {
-        self.0.async_add_machine(Wrapper::Get(m))
-        .map_err(|x| if let Wrapper::Get(c) = x {
-            c
-        } else {
-            unreachable!();
-        })
-    }
-    fn add_timeout_ms(&mut self, delay: u64, t: B::Timeout)
-        -> Result<mio::Timeout, mio::TimerError>
-    {
-        self.0.add_timeout_ms(delay, Wrapper::Get(t))
-    }
-    fn clear_timeout(&mut self, timeout: mio::Timeout) -> bool {
-        self.0.clear_timeout(timeout)
-    }
-    fn register<E: ?Sized>(&mut self, io: &E,
-        interest: mio::EventSet, opt: mio::PollOpt)
-        -> Result<(), io::Error>
-        where E: mio::Evented
-    {
-        self.0.register(io, interest, opt)
-    }
-}
-impl<A:Send, B:Send> rotor::BaseMachine for Wrapper<A, B>
-    where A: rotor::BaseMachine,
-          B: rotor::BaseMachine,
-{
-    type Timeout = Wrapper<A::Timeout, B::Timeout>;
-}
-
-impl<A:Send, B:Send, C> rotor::EventMachine<C> for Wrapper<A, B>
-    where A: rotor::EventMachine<C>,
-          B: rotor::EventMachine<C>,
-{
-    fn ready<S>(self, events: mio::EventSet, context: &mut C, scope: &mut S)
-        -> Option<Self>
-        where S: rotor::Scope<Self>
+impl<C:IncrCounter + GetCounter> rotor::EventMachine<C> for Wrapper<C> {
+    /// Socket readiness notification
+    fn ready(self, events: mio::EventSet, context: &mut C)
+        -> rotor::Async<Self, Option<Self>>
     {
         match self {
-            Wrapper::Incr(i) => i.ready(events, context,
-                &mut ScopeIncr(scope, PhantomData))
-                .map(Wrapper::Incr),
-            Wrapper::Get(i) => i.ready(events, context,
-                &mut ScopeGet(scope, PhantomData))
-                .map(Wrapper::Get),
+            Wrapper::Incr(m) => m.ready(events, context).wrap(Wrapper::Incr),
+            Wrapper::Get(m) => m.ready(events, context).wrap(Wrapper::Get),
         }
     }
-    fn register<S>(&mut self, scope: &mut S)
-        -> Result<(), io::Error>
-        where S: rotor::Scope<Self>
+
+    /// Gives socket a chance to register in event loop
+    fn register(self, reg: &mut rotor::handler::Registrator)
+        -> rotor::Async<Self, ()>
     {
         match self {
-            &mut Wrapper::Incr(ref mut i)
-            => i.register(&mut ScopeIncr(scope, PhantomData)),
-            &mut Wrapper::Get(ref mut i)
-            => i.register(&mut ScopeGet(scope, PhantomData)),
+            Wrapper::Incr(m) => m.register(reg).map(Wrapper::Incr),
+            Wrapper::Get(m) => m.register(reg).map(Wrapper::Get),
         }
     }
-    fn abort<S>(self, reason: rotor::handler::Abort,
-        context: &mut C, scope: &mut S)
-        where S: rotor::Scope<Self>
+
+    /// Timeout happened
+    fn timeout(self, context: &mut C)
+        -> rotor::Async<Self, Option<Self>>
     {
         match self {
-            Wrapper::Incr(i)
-            => i.abort(reason, context, &mut ScopeIncr(scope, PhantomData)),
-            Wrapper::Get(i)
-            => i.abort(reason, context, &mut ScopeGet(scope, PhantomData)),
+            Wrapper::Incr(m) => m.timeout(context).wrap(Wrapper::Incr),
+            Wrapper::Get(m) => m.timeout(context).wrap(Wrapper::Get),
+        }
+    }
+
+    /// Message received
+    fn wakeup(self, context: &mut C)
+        -> rotor::Async<Self, Option<Self>>
+    {
+        match self {
+            Wrapper::Incr(m) => m.wakeup(context).wrap(Wrapper::Incr),
+            Wrapper::Get(m) => m.wakeup(context).wrap(Wrapper::Get),
         }
     }
 }
@@ -201,15 +117,15 @@ fn main() {
     let mut handler = rotor::Handler::new(Context {
         counter: 0,
     }, &mut event_loop);
-    event_loop.channel().send(rotor::handler::Notify::NewMachine(
-        Wrapper::Incr(FSMIncr::new(
+    handler.add_root(&mut event_loop,
+        Wrapper::Incr(HttpServer::new(
             mio::tcp::TcpListener::bind(
                 &"127.0.0.1:8888".parse().unwrap()).unwrap(),
-            )))).unwrap();
-    event_loop.channel().send(rotor::handler::Notify::NewMachine(
-        Wrapper::Get(FSMGet::new(
+            )));
+    handler.add_root(&mut event_loop,
+        Wrapper::Get(HttpServer::new(
             mio::tcp::TcpListener::bind(
                 &"127.0.0.1:8889".parse().unwrap()).unwrap(),
-            )))).unwrap();
+            )));
     event_loop.run(&mut handler).unwrap();
 }
