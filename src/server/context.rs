013d55f35b5dca7544d137e1a91143b261eded25
Paul Colomiets <paul@colomiets.name>
2015-12-29 18:40:58 +0200

    An implementation of default error page

--- src/server/context.rs
+++ src/server/context.rs
@@ -1,6 +1,8 @@
 use hyper::status::StatusCode;
+use hyper::header::{ContentLength, ContentType};
 use rotor_stream::StreamSocket;
 use time::Duration;
+use hyper::mime::{Mime, TopLevel, SubLevel};
 
 use super::Response;
 
@@ -9,7 +11,21 @@ pub trait Context {
     fn emit_error_page(&self,
         code: StatusCode, response: &mut Response)
     {
-        unimplemented!();
+        if response.is_started() {
+            // Too late, nothing to do
+            return;
+        }
+        response.status(code);
+        let data = format!("<h1>{} {}</h1>\n\
+            <p><small>Served for you by rotor-http</small></p>\n",
+            code, code.canonical_reason().unwrap_or("Unknown"));
+        let bytes = data.as_bytes();
+        response.add_header(ContentLength(bytes.len() as u64)).unwrap();
+        response.add_header(ContentType(
+            Mime(TopLevel::Text, SubLevel::Html, vec![]))).unwrap();
+        response.done_headers().unwrap();
+        response.write_body(bytes);
+        response.done();
     }
     fn byte_timeout(&self) -> Duration {
         Duration::seconds(10)
