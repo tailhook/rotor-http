15435e50bb91f0c6d4c13636025f02b77e3b1fd9
Paul Colomiets <paul@colomiets.name>
2016-02-09 02:59:08 +0200

    The is_chunked and is_close helper functions

--- src/headers.rs
+++ src/headers.rs
@@ -39,9 +39,66 @@ pub fn is_connection(val: &str) -> bool {
     return true;
 }
 
+#[inline(always)]
+// header value is byte sequence
+// we need case insensitive comparison and strip out of the whitespace
+pub fn is_close(val: &[u8]) -> bool {
+    if val.len() < "close".len() {
+        return false;
+    }
+    let mut iter = val.iter();
+    for (idx, &ch) in iter.by_ref().enumerate() {
+        match ch {
+            b'\r' | b'\n' | b' ' | b'\t' => continue,
+            b'c' | b'C' => {
+                if idx + "close".len() > val.len() {
+                    return false;
+                }
+                break;
+            }
+            _ => return false,
+        }
+    }
+    for (idx, ch) in iter.by_ref().take(4).enumerate() {
+        if b"lose"[idx] != ch.to_ascii_lowercase() {
+            return false;
+        }
+    }
+    return true;
+}
+
+#[inline(always)]
+// header value is byte sequence
+// we need case insensitive comparison and strip out of the whitespace
+pub fn is_chunked(val: &[u8]) -> bool {
+    if val.len() < "chunked".len() {
+        return false;
+    }
+    let mut iter = val.iter();
+    for (idx, &ch) in iter.by_ref().enumerate() {
+        match ch {
+            b'\r' | b'\n' | b' ' | b'\t' => continue,
+            b'c' | b'C' => {
+                if idx + "chunked".len() > val.len() {
+                    return false;
+                }
+                break;
+            }
+            _ => return false,
+        }
+    }
+    for (idx, ch) in iter.by_ref().take(6).enumerate() {
+        if b"hunked"[idx] != ch.to_ascii_lowercase() {
+            return false;
+        }
+    }
+    return true;
+}
+
 #[cfg(test)]
 mod test {
     use super::{is_content_length, is_transfer_encoding, is_connection};
+    use super::{is_chunked, is_close};
 
     #[test]
     fn test_content_len() {
@@ -65,6 +122,28 @@ mod test {
         assert!(is_connection("CONNECTION"));
         assert!(is_connection("ConneCTION"));
         assert!(is_connection("connection"));
+    }
+
+    #[test]
+    fn test_chunked() {
+        assert!(is_chunked(b"chunked"));
+        assert!(is_chunked(b"Chunked"));
+        assert!(is_chunked(b"chuNKED"));
+        assert!(is_chunked(b"CHUNKED"));
+        assert!(is_chunked(b"   CHUNKED"));
+        assert!(is_chunked(b"   CHUNKED  "));
+        assert!(is_chunked(b"chunked  "));
+    }
+
+    #[test]
+    fn test_close() {
+        assert!(is_close(b"close"));
+        assert!(is_close(b"Close"));
+        assert!(is_close(b"clOSE"));
+        assert!(is_close(b"CLOSE"));
+        assert!(is_close(b" CLOSE"));
+        assert!(is_close(b"   close   "));
+        assert!(is_close(b"Close   "));
     }
 }
 
