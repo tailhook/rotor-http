f129937c1dde03e41b3bc56b19e5a8d48d15256e
Paul Colomiets <paul@colomiets.name>
2016-02-10 16:32:40 +0200

    Fix status duplication in default error page

--- src/server/context.rs
+++ src/server/context.rs
@@ -9,7 +9,7 @@ use super::Response;
 pub trait Context {
     fn emit_error_page(&self, code: StatusCode, response: &mut Response) {
         response.status(code);
-        let data = format!("<h1>{} {}</h1>\n\
+        let data = format!("<h1>{}</h1>\n\
             <p><small>Served for you by rotor-http</small></p>\n",
             code, code.canonical_reason().unwrap_or("Unknown"));
         let bytes = data.as_bytes();
